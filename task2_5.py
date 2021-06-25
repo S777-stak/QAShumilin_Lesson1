93889e294503fc43cf413b7392e08b6d6cd6b24d
S777-stak <sshumilin77@gmail.com>
2021-06-25 19:17:53 +0300

    rename_task2_5

--- task2_5.py
+++ task2_5.py
@@ -1,15 +1,2 @@
-names = ["John Dow", "John Dow", "Marta Dow"]
-
-
-def get_unique_names(names):
-    unique = []
-
-    for name in names:
-        if name in unique:
-            continue
-        else:
-            unique.append(name)
-    return unique
-
-
-print(get_unique_names(names))
+vip = ["John Dow", "John Row", "John Flow"]
+usual = ["Nick Dow", None, "Nick Flow"]
