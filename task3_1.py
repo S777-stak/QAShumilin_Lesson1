552fc4501e7cba255d0f107c135a5dbf371d8179
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-06-28 16:46:38 +0300

    HW3: add comments with description of issue for task 1 and 3

--- task3_1.py
+++ task3_1.py
@@ -7,3 +7,5 @@ for index, value in enumerate(numbers):
     else:
         c = (index, value)
         print(c)
+# Good but you should split you existing numbers list on 2 different list of tuples.
+# All odd indexes with values should be placed in odd_items list and all even in even_items list.
--- task3_3.py
+++ task3_3.py
@@ -1,11 +1,11 @@
-friends = ["John", "Marta", "James"]
+friends = ["James", "John", "Marta"]
 enamies = ["John", "Johnatan", "Artur"]
 
 for friend in friends:
-    if friend == "James":
+    if friend == "James":  # lets say James in start of the list
         break
     else:
-            if friend in enamies:
-                print(f"{friend} we are not the best friends anymore")
-            else:
-                print(f'{friend} we are the best friends')
+        if friend in enamies:
+            print(f"{friend} we are not the best friends anymore")
+        else:
+            print(f'{friend} we are the best friends')
