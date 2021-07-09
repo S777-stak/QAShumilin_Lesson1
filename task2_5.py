438bf88c3eb9488ed5b2f21cd148c9923984e8d5
S777-stak <53911500+S777-stak@users.noreply.github.com>
2021-07-09 12:35:59 +0300

    Delete task2_5.py

--- task2_5.py
+++
@@ -1,5 +0,0 @@
-vip = ["John Dow", "John Row", "John Flow"]
-usual = ["Nick Dow", None, "Nick Flow"]
-
-# Not bad but vip should not mutable. I can easily add some one into the vip
-# group now but should not according to requirements
