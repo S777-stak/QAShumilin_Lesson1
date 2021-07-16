8a0f655cada7d7cb7fda7544e572b9bb0305017e
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-16 22:01:40 +0300

    HW6: add comments with described issues and alternative solutions

--- task6_3.py
+++ task6_3.py
@@ -11,29 +11,11 @@ def is_prime(number):
             return False
     return True
 
-if __name__ == "__main__":
-    assert arithmetic(3, 4, operation="*") == 12
-    assert arithmetic(3, 4, operation="+") == 7
-    assert arithmetic(25, 5, operation="/") == 5
-    assert type(arithmetic(25, 5, operation="/")) == float
-    assert arithmetic(5, 5, operation="//") == f"Not known operation: //"
-    assert arithmetic.__doc__ == (
-        f"\n{' ' * 8}"
-        f"Apply arithmetic operation for provided left and right operands\n"
-        f"{' ' * 4}"""
-    )
-    assert arithmetic.__code__.co_name == "arithmetic"
-    assert arithmetic.__code__.co_varnames == ("left_operand", "right_operand", "operation")
-    try:
-        arithmetic(1, 2, 3)
-    except TypeError as e:
-        assert e.__class__ is TypeError
-    try:
-        arithmetic(left_operand=1, right_operand=2, operation="+")
-    except TypeError as e:
-        assert e.__class__ is TypeError
 
-    try:
-        arithmetic(1, right_operand=2, operation="+")
-    except TypeError as e:
-        assert e.__class__ is TypeError
+if __name__ == '__main__':
+    for item in range(2, 999):
+        print(is_prime(item))
+# Well I suppose something is missing in your code and I getting errors
+# -3 points
+#     for n in range(3, int(sqrt(number).real) + 1, 2):
+# NameError: name 'sqrt' is not defined
