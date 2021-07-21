492bf68b59e83a7790ba4d47b700b5014f520c59
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-22 00:37:19 +0300

    HW7: add comments for task 2

--- task7_2.py
+++ task7_2.py
@@ -5,9 +5,16 @@ def custom_map(callback: Callable, sequence) -> Union[int, float, str]:
     result = []
     for item in sequence:
         if callback(item):
-            result.append(item*5)
+            result.append(item)
     return result
 
 
 if __name__ == '__main__':
-    print(custom_map(lambda item: item, [1, 2, 3, 4, 5, 6, 7]))
+    print(custom_map(lambda item: item * 2, [1, 2, 3, 4, 5, 6, 7]))
+
+# This map does not modify anything. As you can see I am trying to
+# multiple on 2 each item from you list.
+# But inside custom map you just call this callback. But callback for map
+# always should return modified item instead of True or False and you should
+# just place this new modified item into the new sequence
+# -5 points
--- task7_3.py
+++ task7_3.py
@@ -4,9 +4,14 @@ def custom_reduce(callback: Callable, sequence) -> Union[int, float, str]:
     result = []
     for item in sequence:
         if callback(item):
-            result.append(item.__add__(2))
+            result.append(item.__add__(2))  # using magic method not neaded here at all
     return result
 
 
 if __name__ == '__main__':
-    print(custom_reduce(lambda item: item, [1, 2, 3, 4, 5, 6, 7]))
+    print(custom_reduce(lambda a, b: a + b, [1, 2, 3, 4, 5, 6, 7]))
+
+# Well same thing. lambda item: item - this lambda will not do anything but
+# should reduce results and return it.
+# main idea is to call callback and feed 2 arguments until arguments end.
+# -5 points
