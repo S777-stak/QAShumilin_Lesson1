f243178f26efeed60169721ec909ee95d2b302f5
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-22 20:48:11 +0300

    HW8: add comments for task with decorator

--- task8_1.py
+++ task8_1.py
@@ -9,3 +9,12 @@ def sum(a, b):
     return a + b
 
 sum(1, 2)
+
+# Well nice but your decorator brake logic of your function.
+
+result = sum(5, 6)
+print(result)
+# Expected result = 11
+# Actual result = None
+# Decorator should be change main logic of function.
+# - 3 points
--- task8_2.py
+++ task8_2.py
@@ -1,3 +1,6 @@
 new_gen = (item * 2 for item in range(100) if item % 2 == 0)
 for item in new_gen:
     print(item)
+
+# Well done
+# Same could be implemented with generator function
