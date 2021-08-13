bc369512aa0317886a49481e5d394fd4a09df39f
S777-stak <53911500+S777-stak@users.noreply.github.com>
2021-08-13 09:26:56 +0300

    Add files via upload

---
+++ human.py
@@ -0,0 +1,19 @@
+from .action import Action
+
+class Human:
+    def __init__(self, name: str, age: int, action: str):
+        self.__name = name
+        self.__age = age
+        self.__action = Action(action)
+
+    @property
+    def name_human(self) -> str:
+        return self.__name
+
+    @property
+    def age(self) -> int:
+        return self.__age
+
+    @property
+    def action(self) -> Action:
+        return self.__action
