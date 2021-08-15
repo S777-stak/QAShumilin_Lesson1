d2a6abe734607e45781d334e12cd761cdfb6dba3
S777-stak <53911500+S777-stak@users.noreply.github.com>
2021-08-15 17:51:03 +0300

    Add files via upload

---
+++ truck.py
@@ -0,0 +1,14 @@
+
+from Lesson_15.truck_singleton import singleton
+
+@singleton
+class Truck:
+    def __init__(self, color: str, wheel_size: int):
+        self.__color = color
+        self.__wheel_size = wheel_size
+        self.__electro = False
+
+if __name__ == '__main__':
+    truck_1 = Truck("Black", 50)
+    truck_2 = Truck("Brown", 50)
+    print()
