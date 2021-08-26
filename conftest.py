d87e9f4bee6791b628b94377a056a505e23181fb
S777-stak <53911500+S777-stak@users.noreply.github.com>
2021-08-26 13:47:41 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from Lesson_16.function_test.code_for_testing import Human
+
+@pytest.fixture(scope="module")
+
+def human() -> Human:
+    print("Setup for test")
+    yield Human("John", 32, "male")
+
+
+
