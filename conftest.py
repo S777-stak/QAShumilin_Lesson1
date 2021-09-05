6d4fbaa0f219a76c7bc25f99da9ff42ec1e0e718
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-09-05 14:26:47 +0300

    HW16: add comments with described issues

--- conftest.py
+++ conftest.py
@@ -1,9 +1,8 @@
 import pytest
+from code_for_testing import Human
 
-from Lesson_16.function_test.code_for_testing import Human
 
 @pytest.fixture(scope="module")
-
 def human() -> Human:
     print("Setup for test")
     yield Human("John", 101, "male")
